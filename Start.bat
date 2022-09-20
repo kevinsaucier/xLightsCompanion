e92c05bf4d870742e089b3a47d2b3d041cd24f0f
kevinsaucier <kevinsaucier@outlook.com>
2022-09-20 17:17:17 -0400

    Delete Start.bat

--- Start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0xLights-Main.ps1"
-REM Test change
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0xLights-Main.ps1"
