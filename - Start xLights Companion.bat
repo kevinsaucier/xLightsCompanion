eba22c4726f00cccb41e8ae0bdf6f440ce64be00
Kevin Saucier <kevinsaucier@outlook.com>
2022-09-21 17:20:03 -0400

    20220921.3

--- - Start xLights Companion.bat
+++ - Start xLights Companion.bat
@@ -1,6 +1,4 @@
 @echo off
-REM pwsh.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0xLights-Main.ps1"
-REM Test change
 
 REM Specify . as the ShowFolder to use the Script Root
 pwsh.exe -ExecutionPolicy Bypass -File "%~dp0xLights-Main.ps1" -ShowFolder ".\Test Layout"
