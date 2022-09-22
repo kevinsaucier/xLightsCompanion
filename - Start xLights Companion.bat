1cf019ffd7dbc25ea6f32a3bb1fdc3aaae74da1d
Kevin Saucier <kevinsaucier@outlook.com>
2022-09-21 21:58:21 -0400

    2022.09.21.6

--- - Start xLights Companion.bat
+++ - Start xLights Companion.bat
@@ -1,6 +1,27 @@
 @echo off
 
 REM Specify . as the ShowFolder to use the Script Root
-pwsh.exe -ExecutionPolicy Bypass -File "%~dp0xLights-Main.ps1" -ShowFolder ".\Test Layout"
+IF EXIST "C:\Program Files\PowerShell\7\pwsh.exe" GOTO PowerShell7 
+
+
+:PowerShell6
+ECHO Execute Script Using PowerShell 6
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0xLights-Main.ps1" -ShowFolder ".\Test Layout"
+
+GOTO END
+
+
+
+:PowerShell7
+ECHO Execute Script Using PowerShell 7
+pwsh.exe -ExecutionPolicy Bypass -WindowStyle Minimized -File "%~dp0xLights-Main.ps1" -ShowFolder ".\Test Layout" 
+GOTO END
+
+
+ECHO.
+ECHO Script Complete
+ECHO.
+
+:END
 
 REM pause
